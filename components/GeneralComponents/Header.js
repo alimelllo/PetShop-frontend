d01f424d8a05ff006f7491e51540c8e2f486f010
Alireza Maleki <mt@gmail.com>
2023-05-03 21:20:42 +0330

    setting up landing ui

--- components/GeneralComponents/Header.js
+++ components/GeneralComponents/Header.js
@@ -8,13 +8,11 @@ const Header  = ()  => {
     return (
      <div className='flex flex-row justify-between text-white w-full z-50 fixed '>
        
-     <div className="w-3/12 text-center text-[#676767] p-2 text-[1.25rem] ">
-         <div className="w-8/12 md:w-full md:pt-4 rounded-[20px]">
-            <Image src={logo}/>
-         </div>
+     <div className="w-3/12 text-center text-[#bfbfbf] p-2 text-[1.25rem] ">
+       Logo
      </div>
 
-     <div className="w-4/12 flex flex-row justify-center  text-[#676767] text-center pt-3 text-[1.5rem] md:text-[1rem]">
+     <div className="w-4/12 flex flex-row justify-center  text-[#bfbfbf] text-center pt-3 text-[1.5rem] md:text-[1rem]">
         <p className='p-2 mx-2 md:mx-0  cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>تماس </p>
         <p className='p-2 mx-2 md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>محصولات</p>
         <p className='p-2 mx-2 md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>دسته</p>
@@ -23,9 +21,9 @@ const Header  = ()  => {
 
 
 
-     <div className="w-3/12 flex flex-row py-3 justify-end pr-5 text-center text-[#676767] text-[1.25rem]  ">
+     <div className="w-3/12 flex flex-row py-3 justify-end pr-5 text-center text-[#bfbfbf] text-[1.25rem]  ">
      
-       <Link href='/CreateAcount'><p className='text-center md:hidden text-[#676767] pt-3 pr-[2rem] text-[1.25rem] cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[600]'>ثبت نام </p></Link>
+       <Link href='/CreateAcount'><p className='text-center md:hidden text-[#bfbfbf] pt-3 pr-[2rem] text-[1.25rem] cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[600]'>ثبت نام </p></Link>
        <div className="w-[15%] md:w-7/12 relative innerShadow pt-3 p-1 px-2 rounded-[20px] mr-2 md:mr-0">
            <span className="bg-[#ba3131] rounded-[50%] absolute left-[75%] px-2 bottom-[70%] md:bottom-[67%] text-white shadow-2xl md:text-[1rem]">0</span>
             <Image src={Basket} />
--- pages/index.js
+++ pages/index.js
@@ -4,33 +4,36 @@ import { useEffect } from 'react';
 import Header from '../components/GeneralComponents/Header.js';
 import mainBg from '../public/images/main.webp';
 import royalCaninLogo from '../public/images/royalCaninLogo.png';
+import main from '../public/images/main2.png';
+
 
 export default function Home() {
 
 
   return (
    
-    <div className='app font-[BHoma]'>
+    <div className='app font-[BHoma] flex flex-row justify-end'>
        <Head>
         <title>Pet Shop</title>
         <meta property="og:title" content="Pet Shop" key="title" />
       </Head>
     <div>
     
-    <Header />
-    
-        <div className='w-2/12 md:w-6/12 md:top-[10%] md:left-[26%] z-20 left-[5%] top-[15%] rounded-[10px] shadow-lg logoAnimation absolute'>
-              <Image src={royalCaninLogo} />
+    {/* <Header /> */}
+        <div className=''>
+              <Image src={main} />
         </div>
+       
+        {/* 
 
         <div className='w-3/12 z-20 left-[70%] md:left-[10%] md:top-[70%] md:w-10/12 top-[20%] rounded-[10px] absolute '>
             <p className='text-[1.5rem] md:text-[1rem] text-[gray] flex flex-wrap text-justify' dir='rtl'> فروش غذای گربه و سگ و حیوانات خانگی با ضمانت اصالت کالا و تحویل فوری </p>
-            <p className='text-[1.5rem] md:text-[1rem] text-center pb-2 md:py-3 mt-[2rem] w-6/12 md:w-8/12 mx-auto rounded-[10px] transition-all duration-200 hover:scale-105 bg-[#c71f1f] hover:bg-[#980c0c] cursor-pointer  shadow-2xl text-[white]'> محصولات</p>
+            <p className='text-[1.5rem] md:text-[1rem] text-center pb-2 md:py-3 mt-[2rem] w-6/12 md:w-8/12 mx-auto rounded-[10px] transition-all duration-200 hover:scale-105 bg-[#c43a3a] hover:bg-[#9e2d2d] cursor-pointer shadow-xl text-[white]'> محصولات</p>
         </div>
 
         <div className='z-10'>
              <Image src={mainBg} layout='fill' objectFit='contain'/>
-        </div>
+        </div> */}
       </div>
   
 
