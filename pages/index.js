6e64de30014ab7adf69e3806f3e85d029009e84e
alimelllo <ali.melllo@yahoo.com>
2023-05-06 23:41:41 +0330

    testing ui

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import Header from '../components/GeneralComponents/Header.js';
 import mainBg from '../public/images/main.webp';
 import royalCaninLogo from '../public/images/royalCaninLogo.png';
-import main from '../public/images/main2.png';
+import main from '../public/images/main.png';
 
 
 export default function Home() {
@@ -12,20 +12,34 @@ export default function Home() {
 
   return (
    
-    <div className='app font-[BHoma] flex flex-row justify-end'>
+    <div className='app font-[BHoma] flex flex-col'>
        <Head>
         <title>Pet Shop</title>
         <meta property="og:title" content="Pet Shop" key="title" />
       </Head>
     <div>
     
-    {/* <Header /> */}
-        <div className=''>
-              <Image src={main} />
+    <Header />
+
+
+<div className='MAIN-CONTAINER flex flex-row  justify-between w-full'>
+     
+     <div className='flex flex-col w-full text-enter'>
+       <div className='font-[monospace] text-[#4b4b4b] mt-[10rem] text-[4rem] text-center'>
+             Feed us
         </div>
+        <div className='font-[2rem] text-[#a8a8a8] mt-[2rem] text-[3rem] text-center'>
+              فروشگاه غذای حیوانات
+        </div>
+     </div> 
+      
+     <div className='w-full'>
+            <Image src={main} />
+     </div>
+</div>
+        
        
         {/* 
-
         <div className='w-3/12 z-20 left-[70%] md:left-[10%] md:top-[70%] md:w-10/12 top-[20%] rounded-[10px] absolute '>
             <p className='text-[1.5rem] md:text-[1rem] text-[gray] flex flex-wrap text-justify' dir='rtl'> فروش غذای گربه و سگ و حیوانات خانگی با ضمانت اصالت کالا و تحویل فوری </p>
             <p className='text-[1.5rem] md:text-[1rem] text-center pb-2 md:py-3 mt-[2rem] w-6/12 md:w-8/12 mx-auto rounded-[10px] transition-all duration-200 hover:scale-105 bg-[#c43a3a] hover:bg-[#9e2d2d] cursor-pointer shadow-xl text-[white]'> محصولات</p>
