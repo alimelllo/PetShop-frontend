7aee7a379e6ddcb06082e4345b86f207066a6bc3
Alireza Maleki <mt@gmail.com>
2023-05-08 14:10:11 +0330

    adding responsive design to landing

--- components/GeneralComponents/Header.js
+++ components/GeneralComponents/Header.js
@@ -6,23 +6,23 @@ import Link from 'next/link';
 
 const Header  = ()  => {
     return (
-     <div className='flex flex-row w-full justify-between text-white z-50 fixed '>
+     <div className='flex flex-row w-full fixed justify-between text-white z-50'>
 
-     <div className="w-6/12 flex flex-row  text-[#5c5c5c] text-center pt-3 text-[1.5rem] md:text-[1rem]">
-      <div className="w-3/12 ml-5">
+     <div className="w-6/12 flex flex-row  text-[#505050]  text-center pt-3 text-[1.5rem] md:text-[0.8rem]">
+      <div className="w-3/12 ml-5 md:hidden">
         <Image src={logo}/>
       </div>
-        <p className='pt-4 px-3 ml-[10rem] mx-5 hover:shadow-2xl rounded-[20px] md:mx-0  cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>تماس </p>
-        <p className='pt-4 px-3 mx-5 hover:shadow-2xl rounded-[20px] md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>محصولات</p>
-        <p className='pt-4 px-3 mx-5 hover:shadow-2xl rounded-[20px] md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>دسته</p>
+        <p className='py-2 md:py-0 px-5 md:px-4 ml-[10rem] md:ml-0 mx-5 hover:shadow-2xl rounded-[20px] md:mx-0  cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>تماس </p>
+        <p className='py-2 md:py-0 px-5 md:px-4 mx-5 hover:shadow-2xl rounded-[20px] md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'>محصولات</p>
+        <p className='py-2 md:py-0 px-5 md:px-4 mx-5 hover:shadow-2xl rounded-[20px] md:mx-0 cursor-pointer transition-all duration-200 hover:scale-105 hover:text-[#1a1a1a] hover:font-[700] hover:tracking-[1px]'> دسته </p>
      </div>  
 
 
 
 
-     <div className="w-3/12 flex flex-row justify-end pr-5 text-center text-[#bfbfbf] text-[1.25rem]  ">
+     <div className="w-3/12 flex flex-row justify-end pr-5  md:pr-2 text-center text-[#bfbfbf] md:w-5/12">
      
-       <Link href='/CreateAcount'><p className='text-center  text-[#545454] pt-3 px-[2rem] mr-[3rem] mt-5 text-[1.25rem] cursor-pointer transition-all duration-200 hover:scale-105 bg-white shadow-2xl rounded-[25px] hover:text-[#1a1a1a] hover:bg-[#f0f0f0] hover:font-[600]'>ثبت نام  <span>&#8594;</span></p></Link>
+       <Link href='/CreateAcount'><p className='text-center text-[#545454] py-2 md:py-1 px-[2rem] md:px-[1.5rem] mr-[3rem] md:pb-2 md:mr-0 md:text-[1rem] mt-5 md:mt-2 text-[1.25rem] cursor-pointer transition-all duration-200 hover:scale-105 bg-white shadow-2xl rounded-[25px] hover:text-[#1a1a1a] hover:bg-[#f0f0f0] flex flex-row'>ثبت نام  <span>&#8594;</span></p></Link>
        {/* <div className="w-[15%] md:w-7/12 relative innerShadow pt-3 p-1 px-2 rounded-[20px] mr-2 md:mr-0">
            <span className="bg-[#ba3131] rounded-[50%] absolute left-[75%] px-2 bottom-[70%] md:bottom-[67%] text-white shadow-2xl md:text-[1rem]">0</span>
             <Image src={Basket} />
--- pages/index.js
+++ pages/index.js
@@ -4,36 +4,44 @@ import { useEffect } from 'react';
 import Header from '../components/GeneralComponents/Header.js';
 import mainBg from '../public/images/main.webp';
 import royalCaninLogo from '../public/images/royalCaninLogo.png';
-import main from '../public/images/main2.png';
-
+import main from '../public/images/main3.png';
+import catMemoji from '../public/images/memoji.png';
 
 export default function Home() {
 
 
-  return (
-   
-    <div className='app font-[BHoma] flex flex-col'>
+  return (<>
+   <Header/>
+    
+    
+    <div className='app font-[BHoma] flex flex-row md:flex-col-reverse justify-between'>
        <Head>
         <title>Pet Shop</title>
         <meta property="og:title" content="Pet Shop" key="title" />
       </Head>
-    <div>
-    
-    {/* <Header /> */}
-        <div className='w-5/12'>
-              <Image src={main} />
-        </div>
-        <div className='font-[2rem] text-[#a8a8a8] mt-[2rem] text-[3rem] text-center'>
-              فروشگاه غذای حیوانات
-        </div>
-     </div> 
+  
+
+     
       
-     {/* <div className='w-full'>
-            <Image src={main} />
-     </div> */}
+      <div className='w-6/12 md:w-full flex flex-col text-center '>
+          <div className='flex flex-row justify-center mt-[10rem] md:mt-[1rem]'><p className='text-[4rem] md:text-[2.5rem]  font-[600] font-[monospace] text-[#1e2f4b]'>Feed us </p><div className='w-1/12 md:w-2/12 mt-[1.5rem] md:mt-[0.25rem] pl-3'><Image src={catMemoji}/></div></div>
+      
+          <div className='flex flex-row justify-center mt-[2rem] md:mt-[1rem]'><p className='text-[1.75rem] md:text-[1.6rem] text-justify w-5/12 md:w-8/12 text-[#808080]'>فروشگاه غذای حیوانات خانگی با ضمانت اصالت کالا</p></div>
+      
+          <div className='flex flex-row justify-between md:justify-around w-5/12 md:w-full mx-auto'>
+              <button className='bg-gradient-to-r pt-3 md:py-3 w-6/12 from-[#52219b] to-[#771b99] mt-[3rem] text-[white] text-[1.5rem] rounded-[20px] shadow-3xl hover:scale-105 transition-all duration-200'>محصولات</button>
+              <button className='mt-[3rem] pt-2 text-[#241359] text-[1.5rem] rounded-[20px] px-5 hover:shadow-xl hover:scale-105 transition-all duration-200 '>تخفیف ها </button>
+          </div>
+      </div>
+      
 
+      <div className='w-6/12 md:w-full '>
+          <Image src={main} />
+      </div>
 
+       
+        
 
     </div>
-  )
+  </>)
 }
