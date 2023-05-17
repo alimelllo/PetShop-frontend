f91b74e973e1ad885910a5aa0359c1973bbc3d22
Alireza Maleki <mt@gmail.com>
2023-05-17 10:27:44 +0330

    fix vercel deploy

--- components/GeneralComponents/Header.js
+++ components/GeneralComponents/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from 'next/image';
 import Basket from '../../public/images/basket.png';
-import logo from '../../public/images/logo.png';
+import logo from '../../public/images/logo.PNG';
 import Link from 'next/link';
 
 const Header  = ()  => {
