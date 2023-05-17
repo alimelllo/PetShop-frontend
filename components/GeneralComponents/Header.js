bf572d439c05c84bcb432fe3895fba460369d58d
Alireza Maleki <mt@gmail.com>
2023-05-17 10:25:24 +0330

    ...

--- components/GeneralComponents/Header.js
+++ components/GeneralComponents/Header.js
@@ -23,7 +23,7 @@ const Header  = ()  => {
      <div className="w-3/12 flex flex-row justify-end pr-5  md:pr-2 text-center text-[#bfbfbf] md:w-5/12">
      
        <Link href='/CreateAcount'><p className='text-center h-[3rem] text-[#545454]  pt-[10px] md:h-[2.5rem] md:pt-2 md:py-1 px-[2rem] md:px-[1.5rem]  mr-[3rem] md:pb-2 md:mr-0 md:text-[1rem] mt-5 md:mt-3 text-[1.25rem] cursor-pointer transition-all duration-200 hover:scale-105 bg-white shadow-xl rounded-[25px] hover:text-[#1a1a1a] hover:bg-[#f0f0f0] flex flex-row'>ثبت نام  <span className="pl-2">&#8594;</span></p></Link>
-       {/* <div className="w-[15%] md:w-7/12 relative innerShadow pt-3 p-1 px-2 rounded-[20px] mr-2 md:mr-0">
+       {/*   <div className="w-[15%] md:w-7/12 relative innerShadow pt-3 p-1 px-2 rounded-[20px] mr-2 md:mr-0">
            <span className="bg-[#ba3131] rounded-[50%] absolute left-[75%] px-2 bottom-[70%] md:bottom-[67%] text-white shadow-2xl md:text-[1rem]">0</span>
             <Image src={Basket} />
        </div> */}
