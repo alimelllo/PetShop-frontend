05fc22e36d45b738f93fabace4d5305961c2b822
alimelllo <ali.melllo@yahoo.com>
2023-05-28 23:29:01 +0330

    fixing skeleton error

--- components/GeneralComponents/LoadingProductCard.js
+++ components/GeneralComponents/LoadingProductCard.js
@@ -5,7 +5,7 @@ const LoadingProductCard = () => {
     return (
         <div className='w-[18%] flex flex-col justify-around ml-4 h-[15rem] z-20 shadow-2xl my-[1rem]'>
                <div className='w-[90%] mx-auto rounded-[10px] py-5 h-[40%]'>
-                  <Skeleton enableAnimation={true} count={1} height={80} baseColor='#cbcbcb' highlightColor='white' />
+                  <Skeleton  count={1} height={80} baseColor='#cbcbcb' highlightColor='white' />
                </div>
                <div className='w-[50%] pl-3  rounded-[10px] h-[15%] pt-3 '>
                   <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
