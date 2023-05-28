72af449e2340ad0e44460321fc211f9b54293047
Alireza Maleki <mt@gmail.com>
2023-05-28 17:00:07 +0330

    products loading ...

---
+++ components/GeneralComponents/LoadingProductCard.js
@@ -0,0 +1,29 @@
+import Skeleton from 'react-loading-skeleton'
+
+
+const LoadingProductCard = () => {
+    return (
+        <div className='w-[18%] flex flex-col justify-around ml-4 h-[15rem] z-20 shadow-2xl my-[1rem]'>
+               <div className='w-[90%] mx-auto rounded-[10px] py-5 h-[40%]'>
+                  <Skeleton enableAnimation={true} count={1} height={80} baseColor='#cbcbcb' highlightColor='white' />
+               </div>
+               <div className='w-[50%] pl-3  rounded-[10px] h-[15%] pt-3 '>
+                  <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
+               </div>
+               <div className='w-[90%] pl-3  rounded-[10px] h-[15%] '>
+                  <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
+               </div>
+
+               <div className='flex flex-row w-[90%] justify-between  mx-auto'>
+                  <div className='w-[40%] pl-1  rounded-[10px] h-[15%] '>
+                     <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
+                  </div>
+                  <div className='w-[20%] pr-1 rounded-[10px] h-[15%] '>
+                     <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
+                  </div>
+               </div>
+            </div>
+    )
+}
+
+export default LoadingProductCard;
--- pages/Products/index.js
+++ pages/Products/index.js
@@ -1,50 +1,28 @@
 import Header from '../../components/GeneralComponents/Header.js';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import { useEffect, useState } from 'react';
+import LoadingProductCard from '../../components/GeneralComponents/LoadingProductCard.js';
 
 const Products = () => {
 
-   const [loadingProducts, SetLoadingProducts] = useState([]);
-
+   const [ loadingProducts, SetLoadingProducts ] = useState([]);
 
    useEffect(() => {
       const arr = [];
-      for (let i = 0; i < 10; i++) {
-         arr.push(
-            <div className='w-[18%] flex flex-col justify-around ml-4 h-[15rem] z-20 shadow-2xl my-[1rem]'>
-               <div className='w-[90%] mx-auto rounded-[10px] py-5 h-[40%]'>
-                  <Skeleton count={1} height={80} baseColor='#cbcbcb' highlightColor='white' />
-               </div>
-               <div className='w-[50%] pl-3  rounded-[10px] h-[15%] pt-3 '>
-                  <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
-               </div>
-               <div className='w-[90%] pl-3  rounded-[10px] h-[15%] '>
-                  <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
-               </div>
-
-               <div className='flex flex-row w-[90%] justify-between  mx-auto'>
-                  <div className='w-[40%] pl-1  rounded-[10px] h-[15%] '>
-                     <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
-                  </div>
-                  <div className='w-[20%] pr-1 rounded-[10px] h-[15%] '>
-                     <Skeleton count={1} baseColor='#cbcbcb' highlightColor='white' />
-                  </div>
-               </div>
-            </div>
-         )
-      }
+      for (let i = 0; i < 10; i++) {arr.push(<LoadingProductCard/>)}
       SetLoadingProducts(arr);
    }, [])
-
-
 
    return (
       <div>
          <Header />
          <div className='flex flex-row  mt-[3rem]'>
 
-            <div className='PRODUCTS_CONTAINER w-10/12 mt-[2rem] pl-5 border-r-[3px] border-r-[solid] border-r-[#f7f7f7] flex flex-row flex-wrap h-[100%]'>
-               {loadingProducts}
+            <div className='PRODUCTS_CONTAINER w-10/12 mt-[1rem] pl-5 border-r-[3px] border-r-[solid] border-r-[#f7f7f7] flex flex-row flex-wrap h-[100%]'>
+               <div className='bg-[#e8e8e861] shadow-2xl flex flex-row justify-between items-center w-[95%] py-2 mx-auto mt-[2rem]'>
+                  <button className='ml-5 bg-[#5e5e5eb4] rounded-[15px] shadow-xl font-[Bhoma] px-5 h-[3rem] text-white flex justify-center items-center'>جستجو</button>
+                  <input className='w-8/12 h-[3rem] rounded-[15px] shadow-md mr-3 bg-[#b2b2b242]'/>
+               </div>
+                {loadingProducts} 
             </div>
             <div className='FILTER_CONTAINER bg-white w-2/12 shadow-2xl h-screen'>
                nav bar
