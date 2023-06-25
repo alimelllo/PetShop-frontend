b135921358a7142b4fcf227d6a01a3c8f284a052
Alireza Maleki <ali.melllo@yahoo.com>
2023-06-25 16:59:38 +0330

    product view services

--- pages/Products/[ProductView].js
+++ pages/Products/[ProductView].js
@@ -1,10 +1,22 @@
 import Header from "../../components/GeneralComponents/Header";
 import productService from "../../Services/ProductsServices/product.service";
 
-const ProductView = () => {
+const ProductView = (props) => {
+
+    console.log(props);
+
     return (
         <div>
-            <Header/>
+            <Header />
+            <div className="w-full flex flex-row">
+                <div className="w-6/12 bg-[gray] mt-[5rem] flex flex-col">
+                    images
+                </div>
+                <div className="w-6/12 bg-[blue] mt-[5rem] flex flex-col">
+                    <p className="mt-[3rem] text-[gray] "></p>
+                </div>
+            </div>
+
         </div>
     )
 }
@@ -12,7 +24,7 @@ const ProductView = () => {
 export async function getStaticPaths() {
 
     const ProductIds = await productService.getAllProductIds();
-    const loadedPaths =  ProductIds.data.map((item) => ({ params: { ProductView: String(item.id)} }));
+    const loadedPaths = ProductIds.data.map((item) => ({ params: { ProductView: String(item.id) } }));
     return {
         paths: loadedPaths,
         fallback: false
@@ -21,11 +33,14 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const params = context.params.ProductView;
-    // fetch(`fesfdsf/${params}`)
+
+    const imagesResult = await productService.GetAllProductImages(params);
+    const productResult = await productService.getProductById(params);
 
     return {
         props: {
-
+            product: productResult.data,
+            images: imagesResult.data
         }
     }
 }
