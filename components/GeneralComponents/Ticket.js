73e36bb3fa84443150dd5ea626162fc3069322b1
Alireza Maleki <ali.melllo@yahoo.com>
2023-11-22 10:09:25 +0330

    ticket reponsive dots

--- components/GeneralComponents/Ticket.js
+++ components/GeneralComponents/Ticket.js
@@ -8,32 +8,32 @@ const Ticket = ( props ) => {
 
     return (
         <div className={`${props.data.isActive === true ? 'opacity-100' : 'opacity-30'} w-8/12 md:w-11/12 mx-auto font-[bhoma]  bg-gradient-to-tr from-[#2e6edb] to-[#99bdf9] shadow-xl  h-[13rem] md:h-[8rem] relative`}>
-        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'}  absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] top-[5%]`}></span>
-        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] top-[20%]`}></span>
+        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'}  absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] md:left-[-3%]  top-[5%]`}></span>
+        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] md:left-[-3%]  top-[20%]`}></span>
         <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[2.5rem] md:h-[2.5rem] w-[4rem] h-[4rem] rounded-[50%] left-[-6%] md:left-[-8%] top-[35%]`}></span>
-        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] top-[70%]`}></span>
-        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] top-[85%]`}></span>
+        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] md:left-[-3%]  top-[70%]`}></span>
+        <span className={` ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem]  h-[1.5rem]  rounded-[50%] left-[-2%] md:left-[-3%]  top-[85%]`}></span>
 
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[-3%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[5%] `}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[13%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[21%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[29%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[37%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[45%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[53%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[61%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[69%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[78%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[87%]`}></span>
-        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] rounded-[50%] right-[30%] top-[96%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[-3%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[5%] `}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[13%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[21%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[29%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[37%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[45%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[53%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[61%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[69%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[78%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[87%]`}></span>
+        <span className={`opacity-90 ${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute w-[0.9rem] h-[0.9rem] md:w-[0.5rem] md:h-[0.5rem] rounded-[50%] right-[30%] top-[96%]`}></span>
 
 
-        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] top-[5%]`}></span>
-        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] top-[20%]`}></span>
+        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] md:right-[-3%] top-[5%]`}></span>
+        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] md:right-[-3%] top-[20%]`}></span>
         <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[2.5rem] md:h-[2.5rem] w-[4rem] h-[4rem] rounded-[50%] right-[-7%] md:right-[-8%] top-[35%]`}></span>
-        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] top-[70%]`}></span>
-        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] top-[85%]`}></span>
+        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] md:right-[-3%] top-[70%]`}></span>
+        <span className={`${ themeState === 'light' ? 'bg-white' : 'bg-[#141414]'} absolute md:w-[1rem] md:h-[1rem] w-[1.5rem] h-[1.5rem] rounded-[50%] right-[-2%] md:right-[-3%] top-[85%]`}></span>
   
         <p className="absolute tracking-[1px] right-[7%] font-[600] text-[4rem] md:text-[2.5rem] font-[monospace] top-14 md:top-8 text-[white]"> %{props.data.discountPercent}</p>
         <p className="absolute tracking-[1px] right-[40%] md:text-[1.25rem] md:right-[37%] font-[600] text-[1.75rem] top-3 text-[white]">{props.data.title}</p>
