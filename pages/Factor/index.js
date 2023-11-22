68856a1d1d7b116b6c518c6943eae2e74cad6caf
Alireza Maleki <ali.melllo@yahoo.com>
2023-11-22 16:44:41 +0330

    adding final factor page

---
+++ pages/Factor/index.js
@@ -0,0 +1,134 @@
+import Header from '../../components/GeneralComponents/Header';
+
+
+const Factor = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <Header />
+            <div id="DIV_52">
+                <div id="DIV_53">
+                    <div id="DIV_54">
+                        <form action="/mnyfe/secure/purchasev4" method="POST" name="purchaseInfoV4Form" id="FORM_55">
+                            <div id="DIV_1">
+                                <div id="DIV_2">
+                                    <div id="DIV_3">
+                                        <div id="DIV_4">
+                                            <div id="DIV_5">
+                                                <div id="DIV_6">
+                                                    YOUR ORDER
+                                                </div>
+                                                <div id="DIV_7">
+                                                </div>
+                                                <div id="DIV_8">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div id="DIV_9">
+                                            <table id="TABLE_10">
+                                                <tbody id="TBODY_11">
+                                                    <tr id="TR_12">
+                                                        <th id="TH_13">
+                                                            Job Seeker Subscription (Monthly)
+                                                        </th>
+                                                        <td id="TD_14">
+                                                        </td>
+                                                        <td id="TD_15">
+                                                            US$29.99
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_16">
+                                                        <td colspan="3" id="TD_17">
+                                                            <ul id="UL_18">
+                                                                <li id="LI_19">
+                                                                    From September 29, 2014 to October 29, 2014
+                                                                </li>
+                                                            </ul>
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_20">
+                                                        <th id="TH_21">
+                                                            100%-off Promotion
+                                                        </th>
+                                                        <td id="TD_22">
+                                                        </td>
+                                                        <td id="TD_23">
+                                                            (US$29.99)
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_24">
+                                                        <td colspan="3" id="TD_25">
+                                                            <ul id="UL_26">
+                                                                <li id="LI_27">
+                                                                    1 of 1 billing cycles
+                                                                </li>
+                                                            </ul>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                                <tfoot id="TFOOT_28">
+                                                    <tr id="TR_29">
+                                                        <th id="TH_30">
+                                                            Total purchases
+                                                        </th>
+                                                        <td id="TD_31">
+                                                        </td>
+                                                        <td id="TD_32">
+                                                            US$29.99
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_33">
+                                                        <th id="TH_34">
+                                                            Discounts/Credits
+                                                        </th>
+                                                        <td id="TD_35">
+                                                        </td>
+                                                        <td id="TD_36">
+                                                            (US$29.99)
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_37">
+                                                        <th id="TH_38">
+                                                            VAT (0.00%)
+                                                        </th>
+                                                        <td id="TD_39">
+                                                        </td>
+                                                        <td id="TD_40">
+                                                            US$0.00
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_41">
+                                                        <th id="TH_42">
+                                                            Tax (0.00%)
+                                                        </th>
+                                                        <td id="TD_43">
+                                                        </td>
+                                                        <td id="TD_44">
+                                                            US$0.00
+                                                        </td>
+                                                    </tr>
+                                                    <tr id="TR_45">
+                                                        <th id="TH_46">
+                                                            <span id="SPAN_47">Total</span>
+                                                        </th>
+                                                        <td id="TD_48">
+                                                            <span id="SPAN_49"></span>
+                                                        </td>
+                                                        <td id="TD_50">
+                                                            <span id="SPAN_51">US$0.00</span>
+                                                        </td>
+                                                    </tr>
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Factor;
